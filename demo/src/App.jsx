d4868db92fb42863c1f0191c9dd5b2b3f23c5bd6
Mangesh-s5 <mangesh3620@gmail.com>
2026-08-02 13:34:37 +0530

    revise an useState

---
+++ demo/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+//import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>{count}</h1>
+      <br />
+      <br />
+
+      <button onClick={() => setCount(count + 1)}>Radha</button>
+    </>
+  );
+}
+
+export default App;
